71207f8100fe259468cd8ffadd889dd02ccf0054
HAOYU-LI <haoyu@evestemptation.com>
2018-11-24 23:46:51 -0800

    add word auto-complete feature

--- pdf-search-react/src/components/Searchbar.js
+++ pdf-search-react/src/components/Searchbar.js
@@ -1,17 +1,54 @@
 import React from 'react';
-import { Input } from 'antd';
+import { Icon, Input, AutoComplete } from 'antd';
 
-const Search = Input.Search;
+// const Search = Input.Search;
+const Option = AutoComplete.Option;
+
+
+function startWith(k, value) {
+    if (k.length >= value.length) {
+        return k.startsWith(value);
+    }
+    return false;
+}
+
 export class Searchbar extends React.Component {
     state= {
         userInput : 'Input search text',
+        dataSource: [],
     };
 
-    onInputChange = (e) =>{
-        this.setState({userInput : e.target.value});
+
+
+    searchResult = (value) => {
+        let count = 0;
+        const result = [];
+        const dataSource = this.props.dataSource;
+        console.log(dataSource);
+        for (let k in dataSource) {
+            if (startWith(k, value)) {
+                result.push(k)
+                count += 1;
+            }
+
+            if (count >= 8) {
+                break;
+            }
+        }
+
+        return result;
     }
 
-    on_search = () =>  {
+
+    onInputChange = (value) =>{
+        console.log(value);
+        this.setState({userInput : value});
+        this.setState({
+            dataSource: !value ? [] : this.searchResult(value)
+        });
+    }
+
+    onSearch = () =>  {
         console.log(this.state.userInput);
         const strContent = this.state.userInput.toLocaleLowerCase().trim().split(/\s+/);
         //console.log(strContent);
@@ -19,18 +56,58 @@ export class Searchbar extends React.Component {
 
     }
 
+    onSelect = (value) =>{
+        console.log("select");
+    }
+
+
+
+
     render() {
+        const dataIndex =  this.props.datasource;
+
+
+        //
+        // const options = dataIndex.map(item => (
+        //
+        //     <Option className="show-all" key={item.index} value={"word"} >
+        //         <p>
+        //             {item.key}
+        //         </p>
+        //     </Option>
+        //     )
+        // );
+
+
         return (
-            <div className = 'search_button'>
-                <Search
-                    placeholder= {this.state.userInput}
-                    enterButton="Search"
+
+            <div className="search_button">
+                <AutoComplete
+                    className="certain-category-search"
+                    dropdownClassName="certain-category-search-dropdown"
+                    dropdownmatchselectwidth={true}
                     size="large"
-                    onChange = {this.onInputChange}
-                    // onSearch={value => console.log(value)}
-                    onSearch ={this.on_search}
-                />
+                    dataSource={this.state.dataSource}
+                    placeholder={this.state.userInput}
+                    optionLabelProp="value"
+                    onSelect={this.onSelect}
+                    onChange={this.onInputChange}
+                    onSearch={this.handleSearch}
+                >
+                    <Input  suffix={<Icon onClick={this.onSearch} type="search" className="certain-category-icon" />} />
+                </AutoComplete>
             </div>
         );
     }
 }
+
+
+
+{/*<Search*/}
+{/*placeholder= {this.state.userInput}*/}
+{/*enterButton="Search"*/}
+{/*size="large"*/}
+{/*onChange = {this.onInputChange}*/}
+
+{/*onSearch ={this.on_search}*/}
+{/*/>*/}
